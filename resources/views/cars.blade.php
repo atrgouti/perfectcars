d5b4401aa2cb901b1e21da9f82fa60f845b8d6c1
atrgouti <bilalatrgouti2002@gmail.com>
2024-04-21 23:36:06 +0200

    fetching data from database

--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -4,6 +4,38 @@
 Dashboard / Cars
 @endsection
 
-@section("main")
-<h1>this is cars page</h1>
+@section('main')
+<div class="carsArea">
+    <div class="filters">
+        <button>All</button>
+        <button>Avaliable</button>
+        <button>On rent</button>
+    </div>
+    <table border='1'>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Type</th>
+                <th>Year</th>
+                <th>Status</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($carsdata as $car)
+            <tr>
+                <th>1</th>
+                <th>{{$car->name}}</th>
+                <th>{{$car->price}}</th>
+                <th>{{$car->model}}</th>
+                <th>{{$car->year}}</th>
+                <th>{{$car->avaliable}}</th>
+                <th><button>edit</button></th>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/css/dashboard.css">
-    <title>Document</title>
+    <title>PERFECT CARS</title>
+    <link rel="stylesheet" href="/css/dashboard.css">
     <link rel="stylesheet" href="/css/layout.css">
+    <link rel="stylesheet" href="/css/cars.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     
 </head>
